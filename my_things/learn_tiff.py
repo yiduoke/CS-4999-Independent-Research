ffe579744f38eaae8f7b3009b7be399049f428f2
Yiduo Ke <yk467@cornell.edu>
2020-04-23 03:17:55 -0400

    tried out other things; what I have now works best for what I'm about to do

--- my_things/learn_tiff.py
+++ my_things/learn_tiff.py
@@ -18,13 +18,9 @@ for x in range(1, 11):
     im.show()
 '''
     
-file1 = '../MCF10A/1.tif' # dimensions of this tif file are (3, 2048, 2048), class is numpy.uint16
-
+file1 = '../demo_imgs/1.tif' # dimensions of this tif file are (3, 2048, 2048), class is numpy.uint16
 
 file1_matrix = tiff.imread(file1)
-img = scipy.misc.toimage(file1_matrix)
-#plt.imshow(img)
-#plt.show()
 pages, width, height = file1_matrix.shape
 
 # 1st through 3rd pages of the tiff file
@@ -32,17 +28,10 @@ file1_matrix_0 = tiff.imread(file1, key=0)
 file1_matrix_1 = tiff.imread(file1, key=1)
 file1_matrix_2 = tiff.imread(file1, key=2)
 
-# I remember Prof. Hariharan told me tiff file format is AxBx(number of pages), so the number of pages is at the back rather than the front, when you use imread. I have to write it back shifted to fit the tifffile format
-shifted_matrix = np.random.randint(0,2**16, (width,height,pages), type(file1_matrix[0,0,0]))
-shifted_matrix[:,:,0] = file1_matrix_0
-shifted_matrix[:,:,1] = file1_matrix_1
-shifted_matrix[:,:,2] = file1_matrix_2
-print(shifted_matrix)
-print(shifted_matrix.shape)
-tiff.imwrite('file1_blue.tif', file1_matrix_0, photometric='rgb')
-plt.imshow(file1_matrix_0)
+# I remember Prof. Hariharan told me tiff file format is AxBx(number of pages) rather than (number of pages)xAxB
+plt.imshow(file1_matrix_0) # red channel
 plt.show()
-plt.imshow(file1_matrix_1)
+plt.imshow(file1_matrix_1) # green channel
 plt.show()
-plt.imshow(file1_matrix_2)
+plt.imshow(file1_matrix_2) # blue channel
 plt.show()
